37cf696051fbeb5b0b3d52ebf2515edf1d61eca7
Jordan Edoimioya <jordane910@gmail.com>
2022-03-16 02:13:38 +0000

    created class template & function pseudocodes

---
+++ main.py
@@ -0,0 +1,65 @@
+from random import random
+import numpy as np
+import math
+
+
+class NeuralNetwork:
+
+    def __init__(self, num_inputs, num_h_layers, num_outputs):
+        # initialise params
+        # create layer structure
+        # set random weights
+        # store neurons per layer
+        # store derivatives per layer
+        pass
+
+    def sigmoid(self, x):
+        # return output = 1.0 / (1 + np.exp(-x))
+        pass
+
+    def sigmoid_dxdy(self, x):
+        # return x * (1.0 - x)
+        pass
+
+    def fwrd_prop(self, inputs):
+        # neurons (activation) = inputs
+        # iterate through network layers:
+            # matrix mult between previous neurons & weights
+            # apply sigmoid function
+            # store neurons for backprop
+        pass
+
+    def back_prop(self, inputs):
+        # iterate backwards through previous layer
+            # apply sigmoid derivative function
+            # get neurons for current layer
+            # store derivatives after matrix mult
+        pass
+
+    def update_weights(self, learning_param):
+        # loop through all weights:
+            # weights = derivatives * learning_param
+        pass
+
+    def RMSE(self, modelled, observed):
+        # return math.sqrt(np.square(np.subtract(modelled, observed)).mean())
+        pass
+
+    def train(self, inputs, targets, epochs, learning_param):
+        # for i in len(epochs):
+            # iterate through all training data
+            # fwrd_prop()
+            # calc error
+            # back_prop error
+            # update_weights()
+            # RMSE(), append to sum of errors
+        pass
+
+
+if __name__ == "__main__":
+    # import data
+    # create ANN
+    # train ANN
+    # initialise inputs, targets
+    # predict
+    pass
