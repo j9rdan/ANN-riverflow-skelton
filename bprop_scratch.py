be8dfc9cbbaca7d02fcf11d2104b65630961ad8b
Jordan Edoimioya <jordane910@gmail.com>
2022-03-21 17:51:17 +0000

    started backprop

--- bprop_scratch.py
+++ bprop_scratch.py
@@ -5,7 +5,7 @@ class Neuron:
 
     def __init__(self, inputs):
         self.inputs = inputs
-        self.weights = np.random.rand(len(inputs))
+        self.weights = np.random.rand(len(inputs))  # (list)
         self.bias = np.random.rand()
         self.output = 0.0
         self.dw = []
@@ -23,15 +23,30 @@ class NeuralNetwork:
         for layer in self.layers:
             print()
             for neuron in layer:
-                weighted_sum = np.dot(neuron.inputs, neuron.weights) + neuron.bias
-                u = NeuralNetwork.sigmoid(weighted_sum)
+                weighted_sum = np.dot(neuron.inputs, neuron.weights) + neuron.bias  # calc S: weighted sum
+                u = NeuralNetwork.sigmoid(weighted_sum)    # apply sigmoid to weighted sum
+                neuron.output = u   # save output into neuron
                 new_inputs.append(u)
 
         # print(new_inputs)
         return new_inputs
 
     def back_prop(self):
-        
+
+        prev_results = []
+        for i, layer in reversed(list(enumerate(self.layers))):    # start from last layer
+            # print(str(i), ":", self.layers[i])
+            prev_results = []
+            for neuron in self.layers[i-1]:     # for each neuron in the previous layer
+                prev_results.append(neuron.output)
+            # print()
+            # print("previous results:\n", prev_results)
+            for neuron in layer:
+                neuron.inputs = prev_results    # save outputs from previous layer into current
+                print()
+                print(neuron.inputs)
+
+
 
     @staticmethod
     def sigmoid(S):
@@ -56,3 +71,4 @@ l3 = [p1]
 mlp = NeuralNetwork([l1, l2, l3])
 
 mlp.fwrd_prop()
+mlp.back_prop()
