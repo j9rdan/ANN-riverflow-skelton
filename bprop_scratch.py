fca07d81ac810dee9f376a56f4aa55d1477a2de7
Jordan Edoimioya <jordane910@gmail.com>
2022-03-22 00:46:22 +0000

    added some docstrs

--- bprop_scratch.py
+++ bprop_scratch.py
@@ -4,9 +4,15 @@ import numpy as np
 class Neuron:
 
     def __init__(self, inputs):
+
+        """
+        Constructor to create a single neuron within a network's layer
+        :param inputs:  (list) floats either directly from dataset (for input layer), or the output of a neuron in
+        the previous layer (for hidden & output layers)
+        """
         self.inputs = inputs
-        self.weights = np.random.rand(len(inputs))  # (list)
-        self.bias = np.random.rand()
+        self.weights = np.random.rand(len(inputs))  # (list) randomly initialised set of weights
+        self.bias = np.random.rand()   # (float) initialise random bias
         self.output = 0.0
         self.dw = []
         self.db = 0.0
@@ -15,7 +21,15 @@ class Neuron:
 class NeuralNetwork:
 
     def __init__(self, layers):
-        self.layers = layers
+
+        """
+        Constructor to create a neural network by defining a general structure for the input, hidden & output layer
+        :param layers:   (2d list) [layer1, layer2, layer3]
+            layer1 = [i1, i2, ..., i_j] : input layer with j input neurons
+            layer2 = [h1, h2, ..., h_k] : hidden layer with k hidden neurons
+            layer3 = [o1] : output layer with 1 output neuron
+        """
+        self.layers = layers    # list of (list of neuron objects)
 
     def fwrd_prop(self):
 
@@ -23,21 +37,23 @@ class NeuralNetwork:
         for layer in self.layers:
             print()
             for neuron in layer:
-                weighted_sum = np.dot(neuron.inputs, neuron.weights) + neuron.bias  # calc S: weighted sum
-                u = NeuralNetwork.sigmoid(weighted_sum)    # apply sigmoid to weighted sum
-                neuron.output = u   # save output into neuron
-                new_inputs.append(u)
+                weighted_sum = np.dot(neuron.inputs, neuron.weights) + neuron.bias  # calc weighted sum (S)
+                u = NeuralNetwork.sigmoid(weighted_sum)    # apply sigmoid to weighted sum (u=f(S))
+                neuron.output = u   # save output into corresponding neuron
+                new_inputs.append(u)   # store output into list
 
         # print(new_inputs)
         return new_inputs
 
-    def back_prop(self, correct=1):
+    def back_prop(self, correct_output=1):
 
         deltas = []
         for i, layer in reversed(list(enumerate(self.layers))):    # start from last layer
-            for neuron in layer:    # for each neuron in a layer
+            for neuron in layer:
                 derivative = NeuralNetwork.sigmoid_dxdy(neuron.output)  # calc f'(S)
-                delta = (correct - neuron.output) * derivative  #
+                # neuron.dw.append(derivative) ??
+                delta = (correct_output - neuron.output) * derivative  # delta = error * f'(S)
+                print(delta)
                 deltas.append(delta)
 
         # print(deltas)
@@ -56,6 +72,12 @@ class NeuralNetwork:
 
     @staticmethod
     def sigmoid_dxdy(u):
+
+        """
+        Calculates the derivative of the sigmoid activation function for a given activation, u
+        :param u:   (float) value of the activation (i.e. f(S): sigmoid function applied to weighted sum)
+        :return:    (float) derivative output, f'(S)
+        """
         return u * (1.0 - u)
 
 
