6111894493d93549cc13c57266a26bcf7caf6013
Jordan Edoimioya <jordane910@gmail.com>
2022-03-23 00:25:22 +0000

    added update weights function

--- bprop_scratch.py
+++ bprop_scratch.py
@@ -3,7 +3,7 @@ import numpy as np
 
 class Neuron:
 
-    def __init__(self, inputs):
+    def __init__(self, inputs, n_outputs):
 
         """
         Constructor to create a single neuron within a network's layer
@@ -11,10 +11,10 @@ class Neuron:
         the previous layer (for hidden & output layers)
         """
         self.inputs = inputs
-        self.weights = np.random.rand(len(inputs))  # (list) randomly initialised set of weights
+        self.weights = np.random.rand(n_outputs)  # (list) randomly initialised set of weights
         self.bias = np.random.rand()   # (float) initialise random bias
         self.output = 0.0
-        self.dw = []
+        self.delta = 0.0
         self.db = 0.0
 
 
@@ -35,25 +35,25 @@ class NeuralNetwork:
 
         new_inputs = []
         for layer in self.layers:
-            print()
+            # print()
             for neuron in layer:
-                weighted_sum = np.dot(neuron.inputs, neuron.weights) + neuron.bias  # calc weighted sum (S)
+                weighted_sum = sum(np.multiply(neuron.inputs, neuron.weights)) + neuron.bias  # calc weighted sum (S)
                 u = NeuralNetwork.sigmoid(weighted_sum)    # apply sigmoid to weighted sum (u=f(S))
                 neuron.output = u   # save output into corresponding neuron
                 new_inputs.append(u)   # store output into list
 
-        # print(new_inputs)
+        # print("new inputs:\n", new_inputs)
         return new_inputs
 
     def back_prop(self, correct_output=1):
 
         deltas = []
-        for i, layer in reversed(list(enumerate(self.layers))):    # start from last layer
-            for neuron in layer:
+        for i, layer in reversed(list(enumerate(self.layers))):    # work back from last layer
+            for j, neuron in enumerate(layer):
                 derivative = NeuralNetwork.sigmoid_dxdy(neuron.output)  # calc f'(S)
-                # neuron.dw.append(derivative) ??
                 delta = (correct_output - neuron.output) * derivative  # delta = error * f'(S)
-                print(delta)
+                neuron.delta = delta
+                # print("layer", str(j), delta)
                 deltas.append(delta)
 
         # print(deltas)
@@ -80,17 +80,49 @@ class NeuralNetwork:
         """
         return u * (1.0 - u)
 
+    # def update_weights(self, learn_rate):
+    #     for i in range(len(self.layers)):
+    #         if i <= len(self.layers)-1:
+    #             for j, neuron in enumerate(self.layers[i]):
+    #                 delta = self.layers[i+1][j].delta
+    #                 print(delta)
+    #                 for weight in neuron.weights:
+    #                     # print("current weight:", weight)
+    #                     weight += learn_rate * neuron.delta
+    #                     # print("new weight:", str(weight))
+
+    def update_weights(self, learn_rate):
+        for i in range(len(self.layers)):
+            if i < len(self.layers)-1:
+                deltas = []
+                for j in range(len(self.layers[i][0].weights)):
+                    deltas.append(self.layers[i+1][j].delta)
+                print("deltas:", deltas)
+                for neuron in self.layers[i]:
+                    print("current weights:", neuron.weights)
+                    neuron.weights *= neuron.weights * deltas * learn_rate * neuron.output
+                    print("current bias:", neuron.bias)
+                    neuron.bias += learn_rate * neuron.delta
+                    print("new weights:", neuron.weights)
+                    print("new bias:", neuron.bias)
 
 
-p1 = Neuron([1, 1, 1, 1, 1])
-p2 = Neuron([2, 2, 2, 2, 2])
-p3 = Neuron([3, 3, 3, 3, 3])
 
-l1 = [p1, p2]
-l2 = [p1, p2, p3]
-l3 = [p1]
+
+n1 = Neuron([1], 3)
+n2 = Neuron([2], 3)
+n3 = Neuron([3, 3], 1)
+n4 = Neuron([2, 2], 1)
+n5 = Neuron([2, 2], 1)
+n6 = Neuron([2, 2, 2], 1)
+
+
+l1 = [n1, n2]
+l2 = [n3, n4, n5]
+l3 = [n6]
 
 mlp = NeuralNetwork([l1, l2, l3])
 
 mlp.fwrd_prop()
 mlp.back_prop()
+mlp.update_weights(0.5)
